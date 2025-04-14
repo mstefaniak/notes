e340cb670ec22985b923118ccd53f6568237b825
mstefaniak <marcin.stefaniak@plentific.com>
2025-04-14 19:11:49 +0200

    init the React app

--- README.md
+++ README.md
@@ -1,1 +1,1 @@
-Init
+### Notes app
---
+++ src/App.tsx
@@ -0,0 +1,17 @@
+import './App.css'
+
+function App() {
+
+  return (
+    <>
+      <h1>Notes app</h1>
+      <div className="card">
+        <p>
+          Start here ...
+        </p>
+      </div>
+    </>
+  )
+}
+
+export default App
